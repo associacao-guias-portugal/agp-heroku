c459e904597b41ace10d8ac960a03169de08c3c0
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-12-11 16:21:44 +0000

    freguesia no email do formulario

--- services/nodemailer.js
+++ services/nodemailer.js
@@ -17,7 +17,7 @@ const sendNodemailer = (req, res, next) => {
     to: 'publicacoes@guiasdeportugal.org',
     replyTo: `${data.email}`,
     subject: `Pedido de Contacto - Queres ser Guia ?`,
-    html: `<p><b>Recebeu um pedido de contacto através do formulário do site da AGP, pode consultar as informações abaixo.</b></p><div>Email: ${data.email}</div><div>Idade: ${data.idade}</div><div>Concelho: ${data.concelho}</div><div>Telefone: ${data.telefone}</div>`,
+    html: `<p><b>Recebeu um pedido de contacto através do formulário do site da AGP, pode consultar as informações abaixo.</b></p><div>Idade: ${data.idade}</div><div>Concelho: ${data.concelho}</div><div>Freguesia: ${data.freguesia}</div><div>Email: ${data.email}</div><div>Telefone: ${data.telefone}</div>`,
   };
 
   smtpTransporter.sendMail(mailOptions, (error, response) => {
